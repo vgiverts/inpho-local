da7a65d7968e5d316b7463b1ca5aeb2b8b3dece2
Yury Markovsky <yury.markovsky@ymarkovsky-01.workdayinternal.com>
2015-04-09 21:09:14 -0700

    sped up a downloads and uploads to the frame

--- download_and_upload.sh
+++ download_and_upload.sh
@@ -1,12 +1,31 @@
 
-echo $1
+### $1 generally should contain backend_ip
 
-curl $1 > test.jpg
+flashair_ip=192.168.0.1
 
-cp test.jpg FA000001.JPG
+flashair_url="http://${flashair_ip}/upload.cgi"
+backend_url="http://162.243.156.228:9000/img"
 
-echo "uploading FA"
-curl -i -F name=file -F filedata=@FA000001.jpg http://flashair/upload.cgi
+echo "backend = ${backend_url}"
+echo "flashair_url = ${flashair_url}"
 
-echo "uploading test"
-curl -i -F name=file -F filedata=@test.jpg http://flashair/upload.cgi
+curl ${backend_url} > test.jpg
+
+ln -fs test.jpg FA000001.JPG
+
+touch prev.jpg
+
+cmp test.jpg prev.jpg
+
+if [ $? -ne 0 ]; then
+
+    echo "FILE HAS CHANGED ON ${backend_url}, let's upload"
+
+    echo "uploading FA"
+    curl -i -F name=file -F filedata=@FA000001.jpg ${flashair_url}
+    
+    echo "uploading test"
+    curl -i -F name=file -F filedata=@test.jpg ${flashair_url}
+fi
+
+cp test.jpg prev.jpg
